86722664b04dd4fbad0b26126da95cc4e3a06562
KurtisButler <burtiskitler@gmail.com>
2022-01-31 14:19:48 -0600

    added console log and formatted document

--- consolelab.js
+++ consolelab.js
@@ -1,25 +1,12 @@
 const fullName = "Kurtis Duane Butler";
 
-
 const jobTitle = ["Security Guard"];
 const personalDescripton = [
   "I am a male, 6 feet tall, i love playing video games",
 ];
 const interests = ["Games, Anime, Computers, Excercising"];
 
-console.log("Name: " + fullName.toUpperCase());
-console.log("Title: " + jobTitle);
-console.log("Descripton: " + jobTitle);
-console.log(" ")
-console.log("My Interests:")
-
-for (let i = 0; i < interests.length; i++) {
-  console.log("* " + interests[i]);
-  
-}
-
-
-let pastPositions = [
+const pastPositions = [
   {
     Company: "Chickfila",
     Title: "Team Member",
@@ -38,12 +25,37 @@ let pastPositions = [
     Description: "Make sure stuff isnt stolen.",
   },
 ];
-let skills = [
+const skills = [
   "Kitchen Experience",
   "Security Experience",
   "BAM: Computer Knowledge",
   "BAM: Beginner in Web Development",
 ];
+
+console.log("Name: " + fullName.toUpperCase());
+console.log("Title: " + jobTitle);
+console.log("Descripton: " + jobTitle);
+console.log(" ");
+console.log("My Interests:");
+
+for (let i = 0; i < interests.length; i++) {
+  console.log("* " + interests[i]);
+}
+
+console.log(" ");
+
+console.log("My Previous Experience: ");
+
+for (let i = 0; i < pastPositions.length; i++) {
+  console.log(
+    "* " +
+      pastPositions[i].Title +
+      " at " +
+      pastPositions[i].Company +
+      " _ " +
+      pastPositions[i].Description
+  );
+}
 
 function displayPositon(Company, Title, Description) {
   console.log(Company, Title, Description);
@@ -65,6 +77,9 @@ displayPositon(
   pastPositions[2].Title,
   pastPositions[2].Description
 );
+
+console.log(" ");
+console.log("My Skills: ");
 
 displaySkill("Kitchen Experience", false);
 displaySkill("Security Experience", false);
